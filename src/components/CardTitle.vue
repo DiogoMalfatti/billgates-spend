812814bbcac893677a78bf982324b34281efeb80
Diogo Malfatti <diimalfatti@gmail.com>
2021-12-01 20:30:53 -0300

    add imgs

---
+++ src/components/CardTitle.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <img 
+      class="container__img"
+      src="../assets/billgates.jpg"
+      alt="Foto do Bill Gates"
+    />
+    <h1 container__h1>Spend Bill Gates' Money</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CardTitle",
+  };
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .container {
+    width: 980px;
+    background-color: #ffffff;
+    font-size: 32px;
+    padding: 30px 20px;
+    margin-bottom: 10px;
+    font-weight: 700;
+    text-align: center;
+    line-height: 1.3em;
+  }
+  .container__img {
+    user-select: none;
+    width: 125px;
+    height: 125px;
+    margin: 10px auto 25px;
+    border-radius: 125px;
+  }
+  .container__h1 {
+    margin-bottom: 10px;
+  }
+</style>
---
+++ src/components/ProductCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="container__wrapper">
+      <img class="container__img" :src="products.image" :alt="products.name" />
+      <span>{{ products.name }}</span>
+      <span>{{ products.price }}</span>
+    </div>
+    <button>Sell</button>
+    <input type="number" />
+    <button>Buy</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ProductCard",
+    props: {
+      products: Object,
+    },
+  };
+</script>
+
+<style scoped>
+  .container {
+    max-width: 100%;
+    background-color: #fff;
+    text-align: center;
+    font-size: 18px;
+    padding: 20px 15px;
+    width: 100%;
+  }
+  .container__wrapper {
+    display: flex;
+    flex-direction: column;
+  }
+  .container__img {
+    display: block;
+    margin: 15px auto;
+    height: 120px;
+    max-width: 100%;
+    -o-object-fit: contain;
+    object-fit: contain;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+</style>
